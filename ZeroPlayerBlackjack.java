4e348a5d93ec75f1f8edc6c8c2aa78b2995c9980
lcanet11 <Luci.Canet@gmail.com>
2017-10-21 13:20:00 -0400

    Update ZeroPlayerBlackjack.java

--- ZeroPlayerBlackjack.java
+++ ZeroPlayerBlackjack.java
@@ -3,7 +3,20 @@ package com.adampolt;
 public class ZeroPlayerBlackjack {
 
 	public static void main(String[] args) {
-		//Your code here.
+		
+		System.out.println("Welcome to the casino!");
+		// TODO Auto-generated method stub
+		Deck myDeck=new Deck();
+		Card myCard=myDeck.drawCard();
+		int cardValue=myCard.getValue();
+		String cardSuit=myCard.getSuitName();
+		String cardName=myCard.toString();
+		
+		System.out.println("You got a " + cardName);
+		System.out.println("Your total is " + cardValue);
+		
+		Card myCard1=myDeck.nextCard();
+		System.out.println("You got a " + cardName);
+
 	}
 
-}
